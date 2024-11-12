326708a3c07fb19042a152696d8da7ed1137c72a
Rapehue <rapehue@hotmail.com>
2024-11-12 10:22:53 +0100

    Version 2

--- src/bugs/components/MyVerticallyCenteredModal.jsx
+++ src/bugs/components/MyVerticallyCenteredModal.jsx
@@ -1,6 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import { UserCard } from './userCard.jsx';
+import { UserCard } from './UserCard.jsx';
 import { useFetch } from '../../hooks/useFetch.js';
 
 
